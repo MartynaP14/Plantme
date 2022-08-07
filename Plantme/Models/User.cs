97b5839f3ec2fb9683b0e570d991f816dafb87ba
Martyna Martyna <martynapacula@gmail.com>
2022-08-07 22:57:55 +0100

    User model generated

---
+++ Plantme/Models/User.cs
@@ -0,0 +1,35 @@
+﻿namespace Plantme.Models
+{
+    public class User
+    {
+
+        public string UserLoginName { get; set; }
+        public string UserEmail { get; set; }
+
+        public int UserPhoneNo { get; set; }
+
+        public string Location { get; set; }
+
+        public string UserFirstName { get; set; }
+
+        public string UserSurname { get; set; }
+
+        public string Address { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
