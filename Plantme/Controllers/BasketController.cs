c9d6148adddde0583ba4408590c85299a8127954
Martyna Martyna <martynapacula@gmail.com>
2022-11-12 22:41:33 +0000

    Shopping basket created, product to shopping basket addition funtionality added

---
+++ Plantme/Controllers/BasketController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Plantme.Data;
+using Plantme.Models;
+
+namespace Plantme.Controllers
+{
+    public class BasketController : Controller
+    {
+
+
+        private readonly ApplicationDbContext _context;
+
+        public BasketController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+
+        {
+            List<BasketItem> basket = HttpContext.Session.GetJson<List<BasketItem>>("Basket") ?? new List<BasketItem>();
+
+            ShoppingBasket BasketV = new()
+            {
+                BasketItems = basket,
+                TotalAmount = (decimal)basket.Sum(x =>x.ProductQuantity * x.ProductPrice)
+            };
+
+            return View(BasketV);
+        }
+
+        public async Task<IActionResult> Add(int id)
+
+        {
+            Product product = await _context.Products.FindAsync(id);
+
+            List<BasketItem> basket = HttpContext.Session.GetJson<List<BasketItem>>("Basket") ?? new List<BasketItem>();
+
+            BasketItem basketitem = basket.Where(c => c.ProductId == id).FirstOrDefault();
+
+            if (basketitem == null)
+            {
+                basket.Add(new BasketItem(product));
+            }
+            else
+            {
+                basketitem.ProductQuantity += 1;
+            }
+
+            HttpContext.Session.SetJson("Basket", basket);
+
+            TempData["Success"] = "Product was added to your basket";
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+
+    }
+}
---
+++ Plantme/Models/ShoppingBasket.cs
@@ -0,0 +1,11 @@
+﻿namespace Plantme.Models
+{
+    public class ShoppingBasket
+    {
+        public int NumberOfProdcuts { get; set; }
+
+        public List<BasketItem> BasketItems { get; set; }
+        public decimal TotalAmount { get; set; }
+
+    }
+}
