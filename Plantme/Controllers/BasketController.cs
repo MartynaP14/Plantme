3ea7922af12a3603718acf33cc4a5bdec09212e2
Martyna Martyna <martynapacula@gmail.com>
2022-11-15 22:52:20 +0000

    shopping basket funtionality : add, remove, decrement of item implemented, shopping basket added to the navbar

--- Plantme/Controllers/BasketController.cs
+++ Plantme/Controllers/BasketController.cs
@@ -32,7 +32,7 @@ namespace Plantme.Controllers
         public async Task<IActionResult> Add(int id)
 
         {
-            Product product = await _context.Products.FindAsync(id);
+            Models.Product product = await _context.Products.FindAsync(id);
 
             List<BasketItem> basket = HttpContext.Session.GetJson<List<BasketItem>>("Basket") ?? new List<BasketItem>();
 
@@ -57,26 +57,30 @@ namespace Plantme.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(string stripeToken,Guid id )
+        public IActionResult Create(string stripeToken,int id )
         {
-            var product = _context.GetByID(id);
+            //var product = _context.GetByID();
 
-            var charge = new ChargeCreateOptions()
+            List<BasketItem> basket = HttpContext.Session.GetJson<List<BasketItem>>("Basket");
+
+            BasketItem basketitem = basket.Where(c => c.ProductId == id).FirstOrDefault();
+
+            var chargeStripe= new ChargeCreateOptions()
             {
-                Amount = (long)(Convert.ToDouble(product.ProductPrice) * 100),
+                Amount = (long)(Convert.ToDouble(basketitem.ProductPrice) * 100),
                 Currency = "eur",
                 Source = stripeToken,
                 Metadata = new Dictionary<string, string>()
                 {
-                    {"ProductId", product.Id.ToString()},
-                    {"ProductName", product.Name },
+                    {"ProductId", basketitem.ProductId.ToString()},
+                    {"ProductName", basketitem.ProductName },
 
                 }
 
             };
 
             var service = new ChargeService();
-            Charge charge = service.Create(chargeOptions);
+            Charge charge = service.Create(chargeStripe);
 
             if(charge.Status == "succeeded")
             {
@@ -86,6 +90,65 @@ namespace Plantme.Controllers
             return View("Fail");
 
         }
+
+
+        public async Task<IActionResult> Decrease(int id)
+        {
+            List<BasketItem> basket = HttpContext.Session.GetJson<List<BasketItem>>("Basket");
+
+            BasketItem basketitem = basket.Where(c => c.ProductId == id).FirstOrDefault();
+
+            if (basketitem.ProductQuantity > 1)
+            {
+                --basketitem.ProductQuantity;
+            }
+            else
+            {
+                basket.RemoveAll(p => p.ProductId == id);
+            }
+
+            if (basket.Count == 0)
+            {
+                HttpContext.Session.Remove("Basket");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Basket", basket);
+            }
+
+            TempData["Success"] = "The product has been removed from your shopping basket";
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            List<BasketItem> basket = HttpContext.Session.GetJson<List<BasketItem>>("Basket");
+
+            basket.RemoveAll(p => p.ProductId == id);
+
+            if (basket.Count == 0)
+            {
+                HttpContext.Session.Remove("Basket");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Basket", basket);
+            }
+
+            TempData["Success"] = "The product has been removed from your shopping basket";
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Clear()
+        {
+            HttpContext.Session.Remove("Basket");
+
+            return RedirectToAction("Index");
+        }
+
+
 
     }
 }
