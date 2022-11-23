374c94845a70e89334cdfe5f284e8cb74a12fa86
Martyna <102859402+MartynaP14@users.noreply.github.com>
2022-11-23 15:10:17 +0000

    Add files via upload

---
+++ PlantMe.Test/ProductControllerTests.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Plantme.Controllers;
+using Plantme.Data;
+using Plantme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PlantMe_Tests
+{
+    public class ProductControllerTests
+    {
+        
+        public ApplicationDbContext Create_database()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+
+
+                .UseInMemoryDatabase(databaseName: "PlantMeDatabase")
+                .Options;
+            var database = new ApplicationDbContext(options);
+            
+            // seeding database
+            database.Products.Add(new Product
+            {
+                ProductId = 0001,
+                BeginnerFriendly = true,
+                ChildFriendly = false,
+                Description = "house plant, sunny enviroment",
+                DifficultyLevel = "Easy",
+                Documentation = "n/a",
+                GrowingConditions = "Grows in high humid enviroment",
+                PetFriendly = false,
+                Rating = 4,
+                ProductName = "snake plant",
+                ProductTypes = ProductTypes.IndoorPalms,
+                ProductPrice = 15,
+                PlantingInstructions = "n/a",
+                ProductImage = "n/a"
+            });
+            database.Products.Add(new Product
+            {
+                ProductId = 0002,
+                BeginnerFriendly = true,
+                ChildFriendly = false,
+                Description = "house plant, enviroment",
+                DifficultyLevel = "hard",
+                Documentation = "n/a",
+                GrowingConditions = "Grows in hot enviroment",
+                PetFriendly = false,
+                Rating = 5,
+                ProductName = "rose plant",
+                ProductTypes = ProductTypes.IndoorPalms,
+                ProductPrice = 200,
+                PlantingInstructions = "n/a",
+                ProductImage = "n/a"
+            });
+            database.SaveChanges();
+            return database;
+
+        }
+
+        [Fact]
+        public void TestCreate_CreateProduct_WithResponseSuccess()
+        {
+            //Arrange
+            //database created 
+            ApplicationDbContext database = Create_database();
+
+            //controller created
+            ProductsController products_controller = new ProductsController(database);
+
+            Product product = new Product()
+            {
+                ProductId = 0010,
+                BeginnerFriendly = true,
+                ChildFriendly = false,
+                Description = "house plant, enviroment",
+                DifficultyLevel = "hard",
+                Documentation = "n/a",
+                GrowingConditions = "Grows in hot enviroment",
+                PetFriendly = false,
+                Rating = 5,
+                ProductName = "rose plant",
+                ProductTypes = ProductTypes.IndoorPalms,
+                ProductPrice = 200,
+                PlantingInstructions = "n/a",
+                ProductImage = "n/a"
+            };
+
+
+            //Act
+            var result = products_controller.Create(product);
+
+            //Assert
+            Assert.NotNull(result);
+            
+
+
+        }
+
+
+
+
+        [Fact]
+        public void TestDetails_ShowDetailsProduct_WithResponseSuccess()
+        {
+            //Arrange
+            //database created 
+            ApplicationDbContext database = Create_database();
+
+            //controller created
+            ProductsController products_controller = new ProductsController(database);
+
+
+            //Act
+            var result = products_controller.Details(0001);
+
+            //Assert
+            Assert.NotNull(result);
+
+        }
+
+        [Fact]
+        public async void TestDetails_ShowIncorrectDetailsProduct_WithResponseFail()
+        {
+            //Arrange
+            //database created 
+            ApplicationDbContext database = Create_database();
+
+            //controller created
+            ProductsController products_controller = new ProductsController(database);
+
+
+            //Act
+            var result = await products_controller.Details(0011);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+
+        }
+
+
+        [Fact]
+        public async void TestDelete_DeleteProductById_WithResponseFail()
+        {
+            //Arrange
+            //database created 
+            ApplicationDbContext database = Create_database();
+
+            //controller created
+            ProductsController products_controller = new ProductsController(database);
+
+
+            //Act
+            var result = await products_controller.Delete(0011);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+
+        }
+
+        [Fact]
+        public  async void TestDelete_DeleteProductById_WithResponseSuccess()
+        {
+            //Arrange
+            //database created 
+            ApplicationDbContext database = Create_database();
+            database.Products.Add(new Product
+            {
+                ProductId = 0003,
+                BeginnerFriendly = true,
+                ChildFriendly = false,
+                Description = "house plant, enviroment",
+                DifficultyLevel = "hard",
+                Documentation = "n/a",
+                GrowingConditions = "Grows in hot enviroment",
+                PetFriendly = false,
+                Rating = 5,
+                ProductName = "rose plant",
+                ProductTypes = ProductTypes.IndoorPalms,
+                ProductPrice = 200,
+                PlantingInstructions = "n/a",
+                ProductImage = "n/a"
+            });
+            database.SaveChanges();
+
+            //controller created
+            ProductsController products_controller = new ProductsController(database);
+
+
+            //Act
+            var result = await products_controller.Delete(0003);
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+
+        }
+
+    }
+}
---
+++ PlantMe.Test/SeleniumTest.cs
@@ -0,0 +1,19 @@
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace PlantMe_Tests
+{
+    public class SeleniumTest
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+        //    var driver = new ChromeDriver();
+
+        //    driver.Navigate().GoToUrl("https://localhost:7288/");
+
+        //    driver.Quit();
+
+        //}
+    }
+}
